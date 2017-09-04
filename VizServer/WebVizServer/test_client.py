e169175f54795248ac059dfc03883b25a0d1481b
drscotthawley <scott@scotthawley.com>
2017-09-04 17:35:00 -0500

    added a few comments, reordered code

--- VizServer/WebVizServer/test_client.py
+++ VizServer/WebVizServer/test_client.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python3
+
+# Could call this "fake client": it reads from a CSV file called 'sample_data.csv'
+# and sends it to the viz server at the address & port given by host and port,
+# respectively
 from socketIO_client_nexus import SocketIO, LoggingNamespace
 
 import logging
 logging.getLogger('socketIO-client').setLevel(logging.DEBUG)
 logging.basicConfig()
+
+hostname = 'hedges.belmont.edu'
+port = 3000
 
 
 def on_bbb_response(*args):
@@ -11,8 +19,6 @@ def on_bbb_response(*args):
 with open('sample_data.csv', 'r') as myfile:
     data_string =myfile.read()
 
-hostname = 'hedges.belmont.edu'
-port = 3000
 
 with SocketIO(hostname, port, LoggingNamespace) as socketIO:
     #socketIO.emit('chat message', {'xxx': 'yyy'}, on_bbb_response)
